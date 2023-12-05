80d495da989fc7d8e5bd7ebe0be74a491b40aecf
Alex Basurto <alexbasurto1@gmail.com>
2023-12-05 16:48:45 +0100

    modelos BBDD hechos

---
+++ src/models/groupsModel.js
@@ -0,0 +1,86 @@
+import mongoose from "mongoose";
+
+const beneficiaryAndRepartitionSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+});
+
+const transactionSchema = new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    hour: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: String,
+        required: true,
+    },
+    beneficiaryAndRepartition: {
+        type: [beneficiaryAndRepartitionSchema],
+        required: true,
+    },
+});
+
+const groupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 50,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 10,
+        max: 150,
+    },
+    users: {
+        type: Array,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    owner: {
+        type: String,
+        required: true,
+    },
+    transactions: {
+        type: [transactionSchema],
+        required: false,
+    },
+});
+
+const Group = mongoose.model("Group", groupSchema);
+
+export default Group;
---
+++ src/models/usersModel.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 50,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+    mobile: {
+        type: String,
+        required: true,
+        min: 9,
+        max: 9,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 150,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    }
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
